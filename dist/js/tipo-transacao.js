6fc0149f8efd3a41ee4ea946fd2e944757a6dba5
MATEUS CLAUDIO ALVES DA SILVA <mateusclaudio.dev@gmail.com>
2026-06-02 17:06:55 -0300

    Ajuste: configurando .gitignore e removendo node_modules

--- dist/js/tipo-transacao.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var TipoTransacao;
-(function (TipoTransacao) {
-    TipoTransacao["DEPOSITO"] = "Depósito";
-    TipoTransacao["TRANSFERENCIA"] = "Transferência";
-    TipoTransacao["PAGAMENTO_BOLETO"] = "Pagamento de Boleto";
-})(TipoTransacao || (TipoTransacao = {}));
--- dist/js/tipoTransacao.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var TipoTransacao;
-(function (TipoTransacao) {
-    TipoTransacao["DEPOSITO"] = "Dep\u00F3sito";
-    TipoTransacao["TRANSFERENCIA"] = "Transfer\u00EAncia";
-    TipoTransacao["PAGAMENTO_BOLETO"] = "Pagamento de boleto";
-})(TipoTransacao || (TipoTransacao = {}));

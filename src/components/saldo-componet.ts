e381ba77854c02631f19dc5c398e557c0aa66811
Mateus Claudio <mateusclaudio.dev@gmail.com>
2026-06-04 19:31:32 -0300

    add module DataComponent

--- src/components/saldo-componet.ts
+++ src/components/saldo-componet.ts
@@ -1,18 +1,13 @@
 import Conta from "../types/Conta.js";
-import { FormatoData } from "../types/FormatoData.js";
-import { formatarMoeda, formatarData } from "../utils/formatters.js";
+import { formatarMoeda} from "../utils/formatters.js";
 
 
 
 
-const elementoDataAcesso = document.querySelector(".block-saldo time") as HTMLElement;
+
 const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
 
-if (elementoDataAcesso != null) {
-    const dataAcesso = new Date();
-    elementoDataAcesso.textContent = formatarData(Conta.GetDataAcesso(), FormatoData.DIA_SEMANA_DIA_MES_ANO);
-    
-}
+
 
 renderizarSaldo()
 function renderizarSaldo(): void {
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,3 @@
 import "./components/nova-transacao-componet.js";
 import "./components/saldo-componet.js";
+import "./components/data-component.js";
